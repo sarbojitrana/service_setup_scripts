7e4ac7b2dfbf14d67cbaf8ef160f3f08df6ae5c1
sarbojitrana <sarbojitrana47c@gmail.com>
2026-07-04 17:08:28 +0530

    first commit

---
+++ 01_script.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -euo pipefail
+
+echo "Welcome to $USER on $HOSTNAME"
+echo "####################################################"
+
+FREERAM=$(free -m | grep Mem | awk '{print $4}')
+LOAD=`uptime | awk ' {print $9}'`
+ROOTFREE=$(df -h | grep '/dev/sda1' | awk '{print $4}')
+
+
+
+echo "####################################################"
+echo "Available free RAM is $FREERAM MB"
+
+echo "####################################################"
+echo "Current Load Average $LOAD"
+
+echo "####################################################"
+echo "Free ROOT partition size is $ROOTFREE"
